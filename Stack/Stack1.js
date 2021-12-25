d7fc816e4d8bfd9f8f54106773fbd02263885bef
vavellar <victorcandeias@wine.com.br>
2021-12-25 11:49:50 -0300

    feat: stack based in array

---
+++ Stack/Stack1.js
@@ -0,0 +1,48 @@
+class Stack {
+  constructor() {
+    this.items = []
+  }
+
+  push(element) {
+    this.items.push(element)
+  }
+
+  pop() {
+    this.items.pop()
+  }
+
+  isEmpty() {
+    return this.items.length === 0
+  }
+
+  clear() {
+    this.items = []
+  }
+
+  size() {
+    return this.items.length
+  }
+
+  peek() {
+    return this.items[this.size() - 1]
+  }
+}
+
+const stack = new Stack()
+
+console.log(stack.isEmpty())
+stack.push(8)
+stack.push(5)
+
+console.log(stack.peek())
+
+stack.push(11)
+console.log(stack.size())
+console.log(stack.isEmpty())
+
+stack.push(15)
+
+stack.pop()
+stack.pop()
+
+console.log(stack.size())
